a7c1d4839fa2affdb1667f7710c076b594755540
bmetcher <brendan@metcher.net>
2024-10-27 12:24:34 +0800

    player 2 update player 2 now a class & in main.js

--- test/main.js
+++ test/main.js
@@ -1,17 +1,20 @@
 import { $ } from "../lib/Pen.js";
 import { player as player1, player1controls } from "./players/player1.js";
-// import { player2 } from "./players/player2.js";
+import { Player2 } from "./players/player2.js";
 import { player3 } from "./players/player3.js";
 
 const player3item =new player3 ;
+const player2 = new Player2(200, 300);
 
 $.use(update);
+$.debug = true;
 
 function update() {
     $.w = 500
     $.h = 500
+    
     player1controls()
-
+    player2.controls()
     player3item.controls()
 
     $.drawColliders()
--- test/players/player2.js
+++ test/players/player2.js
@@ -1,26 +1,25 @@
 import { $ } from "../../lib/Pen.js";
-export const player2 = new Player2(w/2, h/3);
-
-class Player2 {
+export class Player2 {
     constructor(x, y) {
         this.collider = $.makeCircleCollider(x, y, 20);
+        this.collider.fill = "red";
+        this.collider.friction = 3;
     }
     controls() {
         const max = 30; // set max speed
         // while not max speed, key presses for up/down/left/right
-        while (player2.velocity.x < max && player2.velocity.y < max) {
-            if ($.keys.down("arrowleft")) {
-                player2.velocity.x -= 2;
-            }
-            if ($.keys.down("arrowright")) {
-                player2.velocity.x += 2;
-            }
-            if ($.keys.down("arrowup")) {
-                player2.velocity.y -= 2;
-            }
-            if ($.keys.down("arrowdown")) {
-                player2.velocity.y += 2;
-            }
+        if ($.keys.down("leftarrow")) {
+            this.collider.velocity.x -= 2;
+        }
+        if ($.keys.down("rightarrow")) {
+            this.collider.velocity.x += 2;
+        }
+        if ($.keys.down("uparrow")) {
+            this.collider.velocity.y -= 2;
+        }
+        if ($.keys.down("downarrow")) {
+            this.collider.velocity.y += 2;
         }
     }
 }
+//export const player2 = new Player2(200, 300);
