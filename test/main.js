27b3d1f486613b92bb72e5ca20b418888ff7fc13
hblunt <63094374+hblunt@users.noreply.github.com>
2024-10-27 12:45:31 +1000

    changed main file

--- test/main.js
+++ test/main.js
@@ -1,4 +1,17 @@
 import { $ } from "../lib/Pen.js";
-import { player1 } from "./players/player3.js";
+import { player1 } from "./players/player1.js";
 import { player2 } from "./players/player2.js";
 import { player3 } from "./players/player3.js";
+
+$.use(update);
+
+function update() {
+    $.w = 500
+    $.h = 500
+    player1.player1controls()
+
+    $.drawColliders()
+}
+
+
+
