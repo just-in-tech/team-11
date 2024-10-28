56daedd8229e188c937d566118d2fb036b02d94d
just-in-tech <curlyjustin2001@gmail.com>
2024-10-28 20:20:55 +1000

    corected height and width of canvas

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import { draw_screen as draw_mainmenu } from "./engine/_mainmenu/main.js";
 
 $.use(update);
 function update() {
-    $.w = 500
-    $.h = 500
+    $.w = 900
+    $.h = 900
 
     //draw_mainmenu();
 
