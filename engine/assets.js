a766a029b0479bbb2d1d7e6f34b941dd61f4f4d5
just-in-tech <curlyjustin2001@gmail.com>
2024-10-29 10:43:31 +1000

    add funcation to assets.js to change what is displayed on the screen

---
+++ engine/assets.js
@@ -0,0 +1,15 @@
+import { draw_menuscreen } from "./_mainmenu/main.js";
+
+export function change_gamestate(wantedstate){
+    //remove what is no the screen currently
+
+    //put the wanted thing on the screen
+    if(wantedstate=="mainmenu"){
+        draw_menuscreen()
+    }else if(wantedstate=="creditsscreen"){
+
+    }else if(treemenu){
+
+    }
+
+}
