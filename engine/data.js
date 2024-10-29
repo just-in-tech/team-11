80cabc2a10a123ba432845101f4eb4c05ed59d8f
bmetcher <brendan@metcher.net>
2024-10-29 22:41:54 +0800

    combined old & new data

--- engine/data.js
+++ engine/data.js
@@ -8,36 +8,27 @@ export class Data {
         this.redThings = $.makeGroup();
         this.redThings.name = "redThings";
         this.resources = new Resources();
+        this.gameState = "loading"; // gameState = "loading" | "main menu" | "buildtree" | "battle"
+        // player entities
+        this.playerAnimals = $.makeGroup({  
+            ant: [],
+            eagle: [],
+            bear: []
+        });
+        // enemy entities
+        this.enemyAnimals = $.makeGroup({   
+            ant: [],
+            eagle: [],
+            bear: []
+        })
     }
     update(requests) {
         for (const request of requests) {
             if (request.type == "resource") {
-                this.resources.processRequest(request);
+                this.resources.processRequest(request, this.gameState);
             }
         }
+        // update resources
         this.resources.update();
     }
-    
 }
-
-
-
-/* old data
-export const GameState = {
-    CURRENT: 3, // for testing BATTLE screen
-    LOADING: 0,
-    MAIN_MENU: 1,
-    BUILDTREE: 2, 
-    BATTLE: 3
-};
-
-// PLAYER ANIMAL GROUP(S)
-export const playerAnimals = {
-    ant: [],
-    eagle: [],
-    bear: []
-}
-
-// ENEMY ANIMAL GROUP(S)
-    // how are these added to from 'animalFactory.js' ?
-*/
