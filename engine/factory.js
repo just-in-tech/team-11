ccc464616caf49c992c4a7c90dc0a6464e7b2fec
bmetcher <brendan@metcher.net>
2024-10-31 00:50:58 +0800

    added: makeAnt

--- engine/factory.js
+++ engine/factory.js
@@ -3,28 +3,23 @@ import { $ } from "../lib/Pen.js";
 export class Factory {
     constructor() {
     }
-    // spawn collider with makeThing() method
-    makeThing(x, y, colour) {
-        const temp = $.makeCircleCollider(x, y, 20);
-        temp.fill = colour;
-        temp.direction = $.math.random(0, 360);
-        temp.speed = 5;
-        return temp;
+    //
+    makeAnt(x, y, speed) {   // (feed values like "speed" from data later)
+        const ant = $.makeBoxCollider(x, y, 20, 20);
+        ant.fill = "brown"; // "no fill" possible?
+        ant.friction = 0;
+        ant.direction = 90;
+        ant.speed = speed;
+        return ant;
     }
+    // makeEagle
+    // makeBear
 
     processRequests(requests, data) {
-        if (requests.length > 0) {
-            console.log("Factory saw request: ", requests);
-        }
         for (const request of requests) {
-            // if it is a factory request
             if (request.type == "factory") {
-                // what type of factory request
-                if (request.action == "blue") {
-                    data.blueThings.push(this.makeThing(50, 50, "blue"));
-                }
-                if (request.action == "red") {
-                    data.redThings.push(this.makeThing(100, 50, "red"));
+                if (request.action == "makeAnimal") {
+                    data.playerAnimals.push(this.makeAnt(100, 400, 1));
                 }
             }
         }
