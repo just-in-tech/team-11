32229e5a24b2a00680e2a18d06a12da941f47c31
bmetcher <brendan@metcher.net>
2024-10-31 16:50:06 +0800

    add/update: battleGui & unit creation

--- engine/_battle/battleGui.js
+++ engine/_battle/battleGui.js
@@ -1,20 +1,39 @@
 import { $ } from "../../lib/Pen.js";
 
+let currentFibre;
+
 export class BattleGui {
     constructor() {
-        this.buildTreeButton = $.makeButton(100, 100, 100, 100); // replace later with "surrender" || "return to buildtree"
-        this.buildTreeButton.label = "Return to \n Build Mode";
         // Unit Buttons
-        this.unit
-        this.antButton = $.makeButton(200, 600, 120, 200);
+        this.antButton = $.makeButton(350, 600, 100, 150);
         this.antButton.label = "Buy Ant \n Cost: 3"; // this.cost
         this.antButton.border = "black";
-        // eagle
-        // bear
+
+        this.eagleButton = $.makeButton(500, 600, 100, 150);
+        this.eagleButton.label = "Buy Eagle \n Cost: 6"; // this.cost
+        this.eagleButton.border = "black";
+
+        this.bearButton = $.makeButton(650, 600, 100, 150);
+        this.bearButton.label = "Buy Bear \n Cost: 9"; // this.cost
+        this.bearButton.border = "black";
+        // treemenu button
+        this.treeMenuButton = $.makeButton(100, 100, 100, 100); // replace later with "surrender" || "return to treeMenu"
+        this.treeMenuButton.label = "Return to \n Build Mode";
+        
         this.requests = [];
     }
 
     drawBattle(data) {
+        // Fibre Panel
+        $.colour.fill = "white";
+        $.shape.roundedRectangle(100, 525, 150, 150, 15);
+        currentFibre = String(data.resources.fibre);
+        $.text.size = 28;
+        $.colour.fill = "black";
+        $.text.print(175, 575, "Fibre", 140);
+        $.text.print(175, 625, currentFibre, 140);
+
+        // Buy Animal Buttons
         this.antButton.draw();
         if (data.resources.fibre >= 3) {
             this.antButton.background = "white";
@@ -22,7 +41,8 @@ export class BattleGui {
                 this.requests.push({    // request a player ant
                     type: "factory",
                     action: "makeAnimal",
-
+                    value: "ant",
+                    boolean: 1
                 })
                 this.requests.push({    // fibre cost of 3
                     type: "resource",
@@ -34,11 +54,59 @@ export class BattleGui {
         } else {
             this.antButton.background = "grey";
         }
-
-        this.buildTreeButton.draw(data);
-        if (this.buildTreeButton.up) {
+        this.eagleButton.draw();
+        if (data.resources.fibre >= 6) {
+            this.eagleButton.background = "white";
+            if (this.eagleButton.up) {
+                this.requests.push({    // request a player eagle
+                    type: "factory",
+                    action: "makeAnimal",
+                    value: "eagle",
+                    boolean: 1
+                })
+                this.requests.push({    // fibre cost of 6
+                    type: "resource",
+                    action: "subtractValue",
+                    value: "fibre",
+                    amount: 6
+                })
+            }
+        } else {
+            this.eagleButton.background = "grey";
+        }
+        this.bearButton.draw();
+        if (data.resources.fibre >= 9) {
+            this.bearButton.background = "white";
+            if (this.bearButton.up) {
+                this.requests.push({    // request a player bear
+                    type: "factory",
+                    action: "makeAnimal",
+                    value: "bear",
+                    boolean: 1
+                })
+                this.requests.push({    // fibre cost of 6
+                    type: "resource",
+                    action: "subtractValue",
+                    value: "fibre",
+                    amount: 9
+                })
+            }
+        } else {
+            this.bearButton.background = "grey";
+        }
+        // Tree Menu Button
+        this.treeMenuButton.draw(data);
+        if (this.treeMenuButton.up) {
             data.gameState = "treemenu";
         }
+    }
+
+    endBattle() {
+        // Print Total Silk Gain (& "Continue" Button);
+        // Clear all entities
+        data.resources.fibre = 0;
+        data.gameState = "treemenu";
+        
     }
 
     getRequests() {
--- engine/factory.js
+++ engine/factory.js
@@ -4,21 +4,41 @@ export class Factory {
     constructor() {
     }
     makeAnt(x, y, speed) {   // (feed values like "speed" from data later)
-        const ant = $.makeBoxCollider(x, y, 20, 20);
-        ant.fill = "brown"; // "no fill" possible?
+        const ant = $.makeBoxCollider(x, y, 20, 10);
+        ant.fill = "green"; // "no fill" possible?
         ant.friction = 0;
         ant.direction = 90;
         ant.speed = speed;
         return ant;
     }
-    // makeEagle
-    // makeBear
+    makeEagle(x, y, speed) {   // (feed values like "speed" from data later)
+        const eagle = $.makeBoxCollider(x, y, 10, 20);
+        eagle.fill = "yellow"; // "no fill" possible?
+        eagle.friction = 0;
+        eagle.direction = 90;
+        eagle.speed = speed;
+        return eagle;
+    }
+    makeBear(x, y, speed) {   // (feed values like "speed" from data later)
+        const bear = $.makeBoxCollider(x, y, 20, 40);
+        bear.fill = "red"; // "no fill" possible?
+        bear.friction = 0;
+        bear.direction = 90;
+        bear.speed = speed;
+        return bear;
+    }
 
     processRequests(requests, data) {
         for (const request of requests) {
             if (request.type == "factory") {
                 if (request.action == "makeAnimal") {
-                    data.playerAnimals.push(this.makeAnt(100, 400, 1));
+                    if (request.value == "ant") {
+                        data.playerAnimals.push(this.makeAnt(100, 400, 1));
+                    } else if (request.value == "eagle") {
+                        data.playerAnimals.push(this.makeEagle(100, 400, 1));
+                    } else if (request.value == "bear") {
+                        data.playerAnimals.push(this.makeBear(100, 400, 1));
+                    }   
                 }
             }
         }
