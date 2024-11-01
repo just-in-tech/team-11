41e9268cce23dcf2854cf7736526efb68502d2b3
bmetcher <brendan@metcher.net>
2024-11-01 21:35:55 +0800

    battleManager update

--- engine/_battle/battleManager.js
+++ engine/_battle/battleManager.js
@@ -1,9 +1,41 @@
-import { $ } from "../../lib/Pen";
+import { $ } from "../../lib/Pen.js";
 
 export class BattleManager {
     constructor() {
     }
 
+    battleUpdate() {
+        this.engage(data);
+    }
+
+    engage(data) {
+        for (let i = 0; i < data.playerAnimals; i++) {
+            for (let j = 0; j < data.enemyAnimals; j++) {
+                // Subtract "playerAnimal x" (lower) from "enemyAnimal x" (bigger) as "proximity"
+                let proximity = data.enemyAnimals[j].x - data.playerAnimals[i].x;
+                if (0 < proximity < 100) {
+                    console.log("Spotted!");
+                    data.playerAnimals[i].getAngleToPoint(data.enemyAnimals[j].x, data.enemyAnimals[j].y);
+                    
+                }
+            }
+        }
+        // let's make "proximity" = 2*"range"
+            // base range? or scale with "size"?
+
+        
+
+        // "getAngleToPoint" -- angle to give this entity to face the location
+        // asset -- holds image
+
+        // if collides():
+            // if (attackTimer == 0) {
+            //      attack();
+            //      attackTimer = attackCooldown;
+            // } else if (attackTimer > 0) {
+            //      attackTimer -= 1;
+            // }
+    }
     // attack() {       // defend? how do we reverse? 
         // if (this.timer > 0) {
         //   this.windUpAnimation()  (like, move sprite left for now)
@@ -26,7 +58,7 @@ export class BattleManager {
             // attack()
 
     // function enemyDies() {
-    //      enemy.pull();
+    //      remove()
     //      tokenRNG --
     // }
 }
