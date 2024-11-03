7b375740bcb6f4ce1a52541c5cd319aa800ace81
just-in-tech <curlyjustin2001@gmail.com>
2024-11-03 11:46:57 +1000

    reomved subtract and add requests and changed how battle fibre gen works

--- engine/resources.js
+++ engine/resources.js
@@ -2,22 +2,24 @@ import { $ } from "../lib/Pen.js";
 
 export class Resources {
     constructor() {
-        this.fibre = 0; 
+        this.fibre = 40; 
         this.silk = 0;
+        this.fibrePerInterval=1
+        this.fibreInterval=30
     }
 
     // 'frame-based' resource generation
     update(gameState) {
         if (gameState == "battle") {
-            if ($.frameCount % 60 === 0) {
-                this.fibre += 1;
+            if ($.frameCount % this.fibreInterval === 0) {
+                this.fibre += this.fibrePerInterval;
                 // console.log("Current Fibre:", this.fibre);   // debug: log fibre value each time
             }
         }
     }
 
     // add a resource value
-    gain(resource, cost) {
+   gain(resource, cost) {
         if (request.value == "fibre") {
             this.fibre += request.value;
         } else if (request == "silk") {
@@ -32,13 +34,9 @@ export class Resources {
         }
     }
 
-    processRequest(request) {
-        // console.log(request);   // debug: log any resource request(s)
-        if (request.action == "addValue") {
-            this.gain(request.value, request.amount);
-        }
-        if (request.action == "subtractValue") {
-            this.spend(request.value, request.amount);
-        }
-    }
+
+
+
+
 }
+
