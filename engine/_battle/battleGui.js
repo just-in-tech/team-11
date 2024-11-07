94c6a755acd89ad9c70755639ad4d30d540b2d35
bmetcher <brendan@metcher.net>
2024-11-07 21:30:34 +0800

    chunk of battle update & collider "fixes"

--- engine/_battle/battleGui.js
+++ engine/_battle/battleGui.js
@@ -2,12 +2,16 @@ import { $ } from "../../lib/Pen.js";
 import { Resources } from "../resources.js";
 
 const resources = new Resources();
+
 let currentFibre;
 let currentWave = 0;
 
 export class BattleGui {
     constructor(data) {
-        this.unitButtonY = $.h * (80/100);  // baseline button sizes
+        // asset loading
+        this.battleBackgroundImage = $.loadImage($.w/2, $.h/3, "./engine/_battle/spritesheet/battleBackground.png");
+
+        this.unitButtonY = $.h * (90/100);  // baseline button sizes
         this.unitButtonX = $.w * (20/100);
         // make buttons for user to purchase units
         this.antButton = $.makeButton(this.unitButtonX*2, this.unitButtonY, 100, 150);
@@ -22,52 +26,92 @@ export class BattleGui {
         this.bearButton.label = "Buy BearX \n Cost: " + data.playerStats.bear.priceInGame;
         this.bearButton.border = "black";
         // surrender button during battle
-        this.treeMenuButton = $.makeButton(100, 100, 100, 60);
-        this.treeMenuButton.label = "Surrender";
-        
+        this.surrenderButton = $.makeButton(100, 100, 100, 60);
+        this.surrenderButton.label = "Surrender";
+        this.endButton = $.makeButton($.w/2, $.h/2, $.w/10, $.h/10);
+        this.endButton.label = "Confirm";
+
         this.requests = [];
-        this.battleTimer = 0;
+        this.battleTime = 0;
     }
 
+    drawBattle(data) {
+        this.battleBackgroundImage.draw();
+        // continue drawing the battle until "battleOver" state is true from a lose state
+        if (data.battleOver == true) {
+            this.endBattle(data, resources);
+        } else if (data.battleOver == false) {
+            // update timed events
+            this.battleTimer();
+            resources.generateFibre();
+        }
+        // Draw GUI Elements
+        this.fibrePanel();
+        this.silkPanel();
+        this.unitButtons(data);
+
+        // surrender button
+        this.surrenderButton.draw(data);
+        if (this.surrenderButton.up) {
+            data.battleOver = true;
+        }
+
+        // draw entities
+        $.drawColliders();
+    }
+
+    // keep track of time for battle "events" like enemy spawn
+    battleTimer() {
+        this.battleTime += 1;
+        if (this.battleTime%60 == 0) {
+            if (this.battleTime%1200 == 0) {
+                this.enemySpawn(currentWave);
+                    currentWave++;
+            }
+        }
+    }
+    // spawn enemy waves periodically here
     enemySpawn(currentWave) {
         this.requests.push({
             type: "factory",
             action: "makeAnimal",
             value: "ant",
-            amount: (1 + currentWave),
+            amount: (1 + currentWave%3),
             playerSide: false
         })
     }
 
-    drawBattle(data) {
-        // Update battleTimer
-        this.battleTimer += 1;
-        if (this.battleTimer%60 == 0) {
-            if (this.battleTimer%1200 == 0) {
-                this.enemySpawn(currentWave);
-                currentWave++;
-            }
-        }
-        resources.generateFibre();
-        
-        // Fibre Panel
+    fibrePanel() {
+        // Todo: Add "progress bar" effect
         $.text.alignment.x = "center";
         $.text.alignment.y = "center";
         $.shape.alignment.x = "center"; // doesn't seem to be applying properly
         $.shape.alignment.y = "center";
         $.colour.fill = "white";
-
         $.shape.rectangle(this.unitButtonX, this.unitButtonY, 150, 140, 15);
         currentFibre = String(resources.fibre.fibre);
         $.text.size = 24;
         $.colour.fill = "black";
         $.text.print(this.unitButtonX, this.unitButtonY - 20, "Fibre", 140);
         $.text.print(this.unitButtonX, this.unitButtonY + 20, currentFibre, 140);
+    }
 
-        // Testing
-        $.shape.line(0, $.h/3, $.w, $.h/3);
-        $.shape.line(0, 2*($.h/3), $.w, 2*($.h/3));
+    silkPanel() {
+        // Todo: Add "progress bar" effect
+        $.text.alignment.x = "center";
+        $.text.alignment.y = "center";
+        $.shape.alignment.x = "center"; // doesn't seem to be applying properly
+        $.shape.alignment.y = "center";
+        $.colour.fill = "white";
+        $.shape.rectangle($.w/2, $.h/6, 150, 140, 15);
+        currentFibre = String(resources.silkFromBattle);
+        $.text.size = 24;
+        $.colour.fill = "black";
+        $.text.print($.w/2, $.h/6 - 20, "Silk Collected: ", 140);
+        $.text.print($.w/2, $.h/6 + 20, String(resources.silkFromBattle), 140);
+    }
 
+    unitButtons(data) {
         // Buy Animal Buttons
         this.antButton.draw();
         // show a white version if a unit can be bought (grey if not)
@@ -119,27 +163,42 @@ export class BattleGui {
         } else {
             this.bearButton.background = "grey";
         }
-        // Tree Menu Button
-        this.treeMenuButton.draw(data);
-        if (this.treeMenuButton.up) {
-            this.endBattle(data);
-        }
     }
 
-    endBattle(data) {
-        // Print Total Silk Gain (& "Continue" Button);
+    
+
+    endBattle(data, resources) {
+        $.colour.fill = "white";
+        $.colour.stroke = "black";
+        $.shape.rectangle($.w/2, $.h/3, $.w/4, $.h/4);
+        $.colour.fill = "black";
+        let textHeight = $.h/4;
+        $.text.print($.w/2, textHeight, "Battle Over!", $.w/2);
+        textHeight += $.h/20;
+        $.text.print($.w/2, textHeight, "Silk Total: ", $.w/2);
+        textHeight += $.h/20;
+        // draw icon here
+        let silkTotal = String(resources.silkFromBattle);
+        $.text.print($.w/2, textHeight, silkTotal, $.w/2);
 
         // Clean up animal groups
         for (let i = 0; i < data.playerAnimals.length; i++) {
             data.playerAnimals[i].remove();
         }
-        for (let i = 0; i < data.enemyAnimals.length; i++) {
-            data.enemyAnimals[i].remove();
+        for (let i = 0; i < data.computerAnimals.length; i++) {
+            data.computerAnimals[i].remove();
         }
         // Reset battle values
-        this.battleTimer = 0;
+        this.battleTime = 0;
         resources.fibre.fibre = 0;
-        data.gameState = "treemenu";
+        // Confirm/End Button
+        this.endButton.draw();
+        if (this.endButton.up) {
+            resources.silk = resources.silkFromBattle;
+            resources.silkFromBattle = 0;
+            data.gameState = "treemenu";
+            data.battleOver = false;
+        }
     }
 
     getRequests() {
